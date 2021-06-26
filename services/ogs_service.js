5c276d3cc2da074a7dbf78f68145724c4213b7fa
andrew allred <andrewrallred@gmail.com>
2021-06-26 14:50:37 -0400

    dynamically loading the bearer token rather than from config

--- services/ogs_service.js
+++ services/ogs_service.js
@@ -7,13 +7,41 @@ const Readable = require("stream");
 const fs = require('fs');
 let ws;
 
+async function getBearerToken() {
+
+    let tokenUrl = "https://online-go.com/oauth2/token/";
+
+    const formData = new FormData();
+
+    formData.append("client_id", process.env.OGS_CLIENT_ID);
+    formData.append("client_secret", process.env.OGS_CLIENT_SECRET);
+    formData.append("grant_type", "password");
+    formData.append("username", process.env.OGS_USERNAME);
+    formData.append("password", process.env.OGS_PASSWORD);
+
+    let config = {
+        headers: {
+            ...formData.getHeaders(),
+            "Content-Length": formData.getLengthSync()
+        }
+    }
+
+    const results = await axios.post(tokenUrl, formData, config).catch(function (e) {
+        console.log("error getting token " + e);
+    });
+    
+    return "Bearer " + results.data.access_token;
+
+}
+
 async function getLibrary() {
 
     let libraryUrl = process.env.OGS_LIBRARY_URL.replace("USER_ID", process.env.OGS_USER_ID);
 
+    let bearer = await getBearerToken();
     let config = {
         headers: {
-            "Authorization": process.env.OGS_BEARER
+            "Authorization": bearer
         }
     }
 
@@ -37,17 +65,18 @@ async function uploadSgf(fileName, sgf) {
     fs.writeFileSync(file, sgf);
     formData.append("file", fs.createReadStream(file), { knownLength: fs.statSync(file).size });
 
+    let bearer = await getBearerToken();
     let config = {
         headers: {
             ...formData.getHeaders(),
-            "Authorization": process.env.OGS_BEARER,
+            "Authorization": bearer,
             "Content-Length": formData.getLengthSync()
         }
     }
 
     const results = await axios.post(saveUrl, formData, config).catch(function (e) {
         console.log("error uploading sgf " + e);
-    });;
+    });
     const data = results.data;
 
     console.log("done!");
@@ -59,7 +88,6 @@ async function checkIfGameUploaded(gameName) {
     let library = await getLibrary();
 
     for (let i = 0; i < library.games.length; i++) {
-        //console.log(library.games[i][6]);
         if (library.games[i][6] == gameName || library.games[i][6] == gameName + '.sgf') {
             return true;
         }
